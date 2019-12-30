562d55770b0cbfaa94b75433bdbf6d66f4af070d
wkcn <wkcn@live.cn>
2019-12-30 22:32:01 +0800

    format and update README

--- docs/tutorial/test_mul_func.py
+++ docs/tutorial/test_mul_func.py
@@ -7,12 +7,3 @@ b = mx.nd.array([4, 5, 6])
 out = mx.nd.empty(a.shape)
 mobula.func.mul_elemwise(a.size, a, b, out)
 print(out)  # [4, 10, 18]
-
-#cupy version
-import cupy as cp
-with cp.cuda.Device(0):
-    a = cp.array([1,2,3],dtype='float32')
-    b = cp.array([4,5,6],dtype='float32')
-    out = cp.empty(a.shape,dtype='float32')
-    mobula.func.mul_elemwise(a.size, a, b, out)
-    print(out)
